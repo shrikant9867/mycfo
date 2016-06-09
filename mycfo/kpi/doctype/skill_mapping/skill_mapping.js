364c887a2caf7d758993ca79c7c8d8afcccd9495
sbkolate <kolate.sambhajI@gmail.com>
2016-06-09 11:40:07 +0530

    removed from and to date in Skill mapping year of exp.

--- mycfo/kpi/doctype/skill_mapping/skill_mapping.js
+++ mycfo/kpi/doctype/skill_mapping/skill_mapping.js
@@ -4,38 +4,38 @@ cur_frm.add_fetch('sub_industry', 'industry', 'industry');
 cur_frm.add_fetch("employee", "user_id", "user_id");
 
 //calculate year of year_of_experience in all three tables
-frappe.ui.form.on("Previouse Employer Details", "from", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
-frappe.ui.form.on("Previouse Employer Details", "to", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
+// frappe.ui.form.on("Previouse Employer Details", "from", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
+// frappe.ui.form.on("Previouse Employer Details", "to", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
 
-frappe.ui.form.on("Previous Employer Project Details", "from", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
-frappe.ui.form.on("Previous Employer Project Details", "to", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
+// frappe.ui.form.on("Previous Employer Project Details", "from", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
+// frappe.ui.form.on("Previous Employer Project Details", "to", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
 
-frappe.ui.form.on("MyCFO Projects Experience", "from", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
-frappe.ui.form.on("MyCFO Projects Experience", "to", function(frm,cdt,cdn) {
-  var d = locals[cdt][cdn];
-  var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
-  frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
-});
+// frappe.ui.form.on("MyCFO Projects Experience", "from", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
+// frappe.ui.form.on("MyCFO Projects Experience", "to", function(frm,cdt,cdn) {
+//   var d = locals[cdt][cdn];
+//   var a = (Date.parse(d.to)-Date.parse(d.from))/(1000 * 3600 * 24*30);
+//   frappe.model.set_value(cdt, cdn, "year_of_experience", (a/12).toFixed(1));
+// });
 
 frappe.ui.form.on("Skill Mapping", "validate", function(frm) {
       if(frm.doc.previous_employer_details){
