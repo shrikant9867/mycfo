6a1bb49121412a98027142dd83788dd1c5ff63ec
suyashphadtare <suyash.p@indictranstech.com>
2017-01-09 19:52:40 +0530

    [Feature] Customer OM Details Report

---
+++ mycfo/mycfo/report/customer_operational_matrix_details/customer_operational_matrix_details.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2013, Indictrans and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Customer Operational Matrix Details"] = {
+	"filters": []
+}
